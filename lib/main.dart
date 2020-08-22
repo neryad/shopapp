f3375d90dfde9667986ed995bd0b027e035335c0
Neryad <dayern.gomez@hotmail.com>
2020-08-22 09:17:26 -0400

    name changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TuCompra',
+      title: 'Pockedlist',
       home: Scaffold(
         body: HomePage(),
       ),
