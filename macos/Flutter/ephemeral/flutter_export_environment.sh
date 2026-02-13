9ce32822bc2e9a47d60559f5f500fd88654a049b
neryad <dayern.gomez@hotmail.com>
2026-02-12 21:47:10 -0400

    feat: Implement new list creation and management with product details, budget tracking, and total calculation, alongside initial Flutter project setup.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\dev\shopapp"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\dev\shopapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.0.0"
